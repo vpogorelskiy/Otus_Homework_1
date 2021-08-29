55b9fc1a3ed04a89f3c3aaf241ba614fa6318e3e
Vyacheslav Pogorelskiy <>
2021-08-29 09:32:27 +0300

    Added base view structure

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,14 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var selectedTab: Int = 0
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
+        TabView(selection: $selectedTab) {
+            FirstView(selection: $selectedTab).tabItem {
+                TabItem(imageName: "square", text: "First")
+            }
+            
+            SecondView().tabItem {
+                TabItem(imageName: "star", text: "Second")
+            }
+            
+            ThirdView().tabItem {
+                TabItem(imageName: "triangle", text: "Third")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct TabItem: View {
+    @State var imageName: String
+    @State var text: String
+    var body: some View {
+        VStack {
+            Image(systemName: imageName)
+            Text(text)
+        }
+    }
+}
+
+struct FirstView: View {
+    
+    @Binding var selection: Int
+    
+    var body: some View {
+        Button("Go to second") {
+            selection = 1
+        }
+    }
+}
+
+struct SecondView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                Text("One")
+                Text("Two")
+                Text("Three")
+            }
+        }
+    }
+}
+
+struct ThirdView: View {
+    var body: some View {
+        Button("Open modal") {
+            // TODO: Implement
+        }
     }
 }
